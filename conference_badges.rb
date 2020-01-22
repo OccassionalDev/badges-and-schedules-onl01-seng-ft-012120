bb695e4c78724ac16cd3cac49dd157427fd94772
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-22 16:45:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,10 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(name_list)
+  badge_messages = []
+  name_list.each do |name|
+    badge_messages += "Hello, my name is #{name}."
+  end
+end

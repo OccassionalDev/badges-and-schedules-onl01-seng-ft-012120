c490e76e6a212354af9890178fa6cecc3defab7b
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-22 16:52:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,16 +6,22 @@ end
 
 def batch_badge_creator(name_list)
   badge_messages = []
+  
   name_list.each do |name|
     badge_messages.push("Hello, my name is #{name}.")
   end
+  
   return badge_messages
 end
 
 def assign_rooms(list_of_speakers)
   list_room_assignments = []
+  
   list_speakers_of_speakers.each_with_index do |speaker, room_number|
+    
     list_room_assignments.push("Hello, #{speaker}! You'll be assigned to room #{(room_number+1).to_s}")
   end
+  
+  return list_room_assignments
   
 end
